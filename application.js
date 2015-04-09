5f3b87b87e826431f73d90f58defd9bcef5e166d
Mark Lilien <mark@thegiantpixel.com>
2015-04-08 18:00:29 -0700

    validated login and hide login/show chat room on login success

--- application.js
+++ application.js
@@ -14,14 +14,59 @@ function setupLogin() {
 
 function handleLoginKeyDown(evt) {
     if(evt.keyCode == Rapp.KEY_CODES.ENTER){
-        console.log("hello world");
-        
-    }else if(evt.keyCode == Rapp.KEY_CODES.DELETE){ 
-        console.log("test delete")
-    }else {
-        console.log (evt.keyCode);
-        
+        login();
     }
 }
 
-$(document).ready(pageSetup)
+function login(){
+    var username;
+    var room_name;
+    
+    username = $("#username-input").val(); 
+    username = $.trim(username);
+    room_name = $("#room-name-input").val();
+    room_name = $.trim(room_name);
+    
+    if(!username){
+        alert("YOU NEED A USERNAME!!!!!!!");
+    }else if(!room_name){
+        alert("YOU NEED A ROOM NAME !!!!!!!")
+    }else {
+        initializeChat()
+    }
+}
+
+function initializeChat(){
+    $("#page-frame").addClass("chat-mode");    
+}
+
+$(document).ready(pageSetup);
+
+/*
+function multiply(x,y){
+    var result;
+    var counter;
+    result = 0;
+    counter = 0;
+    while(counter<y){
+        result = result + x;
+        counter = counter + 1;
+    
+    }
+    return result;
+}
+
+var twelve = multiply(3,4);
+var juan = multiply(58936,395824); 
+alert(twelve);
+alert(juan);
+*/
+
+
+
+
+
+
+
+
+
